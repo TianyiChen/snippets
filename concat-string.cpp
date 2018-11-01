cb335368ec8b77d5961bd2076902cc55fa769bb0
Tianyi Chen <TianyiChen@users.noreply.github.com>
2018-11-01 20:51:18 +0800

    Update concat-string.cpp

--- concat-string.cpp
+++ concat-string.cpp
@@ -1,13 +1,13 @@
 inline void _concat_string(string&){}
 template<class T,class...R>
 inline void _concat_string(string&s,const T&t,const R&...r){
-	if constexpr(is_arithmetic_v<remove_const_t<remove_reference_t<T>>>)s+=to_string(t);
+	if constexpr(is_arithmetic_v<remove_const_t<remove_reference_t<T>>>&&!is_same_v<T,char>)s+=to_string(t);
 	else s+=t;
 	_concat_string(s,r...);
 }
 template<class T,class...R>
 inline string concat_string(const T&t,const R&...r){
-	if constexpr(is_arithmetic_v<remove_const_t<remove_reference_t<T>>>){
+	if constexpr(is_arithmetic_v<remove_const_t<remove_reference_t<T>>>&&!is_same_v<T,char>){
 		auto rt=to_string(t);
 		_concat_string(rt,r...);return rt;
 	} else{
